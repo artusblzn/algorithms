248a1e7711a23a257b7a97a3a2cdc208af92c124
Artus Bolzanni <artusb@tutanota.com>
2023-05-15 13:47:38 -0300

    Changed some int variables to bool.

--- algorithms_and_data_structures/data_structures/stack/include/stack.h
+++ algorithms_and_data_structures/data_structures/stack/include/stack.h
@@ -1,6 +1,8 @@
 #ifndef STACK_H
 #define STACK_H
 
+#include <stdbool.h>
+
 typedef struct stack {
   int *items;
   int top;
@@ -9,10 +11,10 @@ typedef struct stack {
 
 Stack *__stack__(int stack_capacity);
 void push(Stack *stack, int item);
-int stack_is_full(Stack *stack);
+bool stack_is_full(Stack *stack);
 int pop(Stack *stack);
-int stack_is_empty(Stack *stack);
-void push_and_test(Stack *stack, int item, int *stack_overflow);
-int pop_and_test(Stack *stack, int *stack_underflow);
+bool stack_is_empty(Stack *stack);
+void push_and_test(Stack *stack, int item, bool *stack_overflow);
+int pop_and_test(Stack *stack, bool *stack_underflow);
 
 #endif
